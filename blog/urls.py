cb245595c7af6903624d7dfc80d8b72f5c26429e
anaselhafid <anas.elhafid@gmail.com>
2022-05-31 10:47:57 +0100

    ajout de post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 urlpatterns = [
 	path('',views.post_list,
 		name='post_list'),
+        #path('bonjour',views.bonjour,
+	#	name='bonjour'),
+        path('post/<int:pk>/',views.post_detail,
+		name='post_detail'),
+        
 ]
